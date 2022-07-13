d74ebcd12b17f869b1b39377730af075367f6b47
pablo <mendez4a@gmail.com>
2022-07-13 15:50:43 +0200

    Implement web3signer support

---
+++ beacon-chain/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ -n $WEB3_BACKUP ]] && [[ $EXTRA_OPTS != *"--fallback-web3provider"* ]]; then
+  EXTRA_OPTS="--fallback-web3provider=${WEB3_BACKUP} ${EXTRA_OPTS}"
+fi
+
+if [[ -n $CHECKPOINT_SYNC_URL ]]; then
+  EXTRA_OPTS="--checkpoint-sync-url=${CHECKPOINT_SYNC_URL} --genesis-beacon-api-url=${CHECKPOINT_SYNC_URL} ${EXTRA_OPTS}"
+else
+  EXTRA_OPTS="--genesis-state=/genesis.ssz ${EXTRA_OPTS}"
+fi
+
+exec -c beacon-chain \
+  --datadir=/data \
+  --rpc-host=0.0.0.0 \
+  --accept-terms-of-use \
+  --mainnet \
+  --grpc-gateway-host=0.0.0.0 \
+  --monitoring-host=0.0.0.0 \
+  --http-web3provider=$HTTP_WEB3PROVIDER \
+  --grpc-gateway-port=3500 \
+  --grpc-gateway-corsdomain=$CORSDOMAIN \
+  $EXTRA_OPTS
--- validator/entrypoint.sh
+++ validator/entrypoint.sh
@@ -1,57 +1,62 @@
 #!/bin/bash
 
-# Paths
-AUTH_TOKEN_DIR="/root/.eth2validators"
-AUTH_TOKEN_FILE="${AUTH_TOKEN_DIR}/auth-token"
-TOKEN=""
+CLIENT="prysm"
+export NETWORK="mainnet"
+VALIDATOR_PORT=3500
+export WEB3SIGNER_API="http://web3signer.web3signer.dappnode:9000"
+export WALLET_DIR="/root/.eth2validators"
 
-# Generate JWT if does not exist already
-if [ ! -f "${AUTH_TOKEN_FILE}" ]; then
-    echo "[INFO] generating JWT..."
-    mkdir -p "${AUTH_TOKEN_DIR}"
-    # --wallet-dir value   Path to a wallet directory on-disk for Prysm validator accounts (default: "/root/.eth2validators/prysm-wallet-v2")
-    validator web generate-auth-token --wallet-dir=${AUTH_TOKEN_DIR} --accept-terms-of-use || { echo "[ERROR] failed to generate JWT"; exit 1; }
-fi
+# Copy auth-token in runtime to the prysm token dir
+mkdir -p ${WALLET_DIR}
+cp /auth-token ${WALLET_DIR}/auth-token
 
-# Print token
-TOKEN=$(sed -n 2p ${AUTH_TOKEN_FILE}) || { echo "[ERROR] failed to read JWT"; exit 1; }
-echo "[INFO] successfully detected JWT: ${TOKEN}"
-
-# Check if the token exists and post 
-# the url with the token to the dappmanager
-if [ ! -z "$TOKEN" ]; then
-    # Post JWT to dappmanager
-    curl --connect-timeout 5 \
-        --max-time 10 \
-        --retry 5 \
-        --retry-delay 0 \
-        --retry-max-time 40 \
-        -X POST "http://my.dappnode/data-send?key=token&data=http://prysm.dappnode/initialize?token=${TOKEN}" \
-        || { echo "[ERROR] failed to post JWT to dappmanager"; exit 1; }
-
+# Migrate if required
+if [[ $(validator accounts list \
+    --wallet-dir="$WALLET_DIR" \
+    --wallet-password-file="${WALLET_DIR}/walletpassword.txt" \
+    --mainnet \
+    --accept-terms-of-use) ]]; then
+    {
+        echo "found validators, starging migration"
+        eth2-migrate.sh &
+        wait $!
+    }
 else
-    { echo "[ERROR] could not find auth token file"; exit 1; }
+    { echo "validators not found, no migration needed"; }
 fi
 
-# Check vars 
-[ -z "$BEACON_RPC_PROVIDER" ] && { echo "[ERROR] BEACON_RPC_PROVIDER is not set"; exit 1; } || echo "[INFO] BEACON_RPC_PROVIDER ${BEACON_RPC_PROVIDER}"
-[ -z "$BEACON_RPC_GATEWAY_PROVIDER" ] && { echo "[ERROR] BEACON_RPC_GATEWAY_PROVIDER is not set"; exit 1; } || echo "[INFO] BEACON_RPC_GATEWAY_PROVIDER ${BEACON_RPC_GATEWAY_PROVIDER}"
-[ -z "$GRAFFITI" ] && echo "[WARN] GRAFFITI is not set" || echo "[INFO] GRAFFITI ${GRAFFITI}"
+# Remove manual migration if older than 20 days
+find /root -type d -name manual_migration -mtime +20 -exec rm -rf {} +
 
-# Must used escaped \"$VAR\" to accept spaces: --graffiti=\"$GRAFFITI\"
-exec -c validator \
-  --mainnet \
-  --datadir=/root/.eth2 \
-  --rpc-host 0.0.0.0 \
-  --monitoring-host 0.0.0.0 \
-  --beacon-rpc-provider="$BEACON_RPC_PROVIDER" \
-  --beacon-rpc-gateway-provider="$BEACON_RPC_GATEWAY_PROVIDER" \
-  --wallet-dir=/root/.eth2validators \
-  --wallet-password-file=/root/.eth2wallets/wallet-password.txt \
-  --write-wallet-password-on-web-onboarding \
-  --graffiti="$GRAFFITI" \
-  --web \
-  --grpc-gateway-host=0.0.0.0 \
-  --grpc-gateway-port=80 \
-  --accept-terms-of-use \
-  ${EXTRA_OPTS}
+WEB3SIGNER_RESPONSE=$(curl -s -w "%{http_code}" -X GET -H "Content-Type: application/json" -H "Host: validator.${CLIENT}.dappnode" "${WEB3SIGNER_API}/eth/v1/keystores")
+HTTP_CODE=${WEB3SIGNER_RESPONSE: -3}
+CONTENT=$(echo "${WEB3SIGNER_RESPONSE}" | head -c-4)
+
+if [ "${HTTP_CODE}" == "403" ] && [ "${CONTENT}" == "*Host not authorized*" ]; then
+    echo "${CLIENT} is not authorized to access the Web3Signer API. Start without pubkeys"
+elif [ "$HTTP_CODE" != "200" ]; then
+    echo "Failed to get keystores from web3signer, HTTP code: ${HTTP_CODE}, content: ${CONTENT}"
+else
+    PUBLIC_KEYS_WEB3SIGNER=($(echo "${CONTENT}" | jq -r 'try .data[].validating_pubkey'))
+    if [ ${#PUBLIC_KEYS_WEB3SIGNER[@]} -gt 0 ]; then
+        PUBLIC_KEYS_COMMA_SEPARATED=$(echo "${PUBLIC_KEYS_WEB3SIGNER[*]}" | tr ' ' ',')
+        echo "found validators in web3signer, starting vc with pubkeys: ${PUBLIC_KEYS_COMMA_SEPARATED}"
+        EXTRA_OPTS="--validators-external-signer-public-keys=${PUBLIC_KEYS_COMMA_SEPARATED} ${EXTRA_OPTS}"
+    fi
+fi
+
+exec -c validator --mainnet \
+    --datadir="$WALLET_DIR" \
+    --wallet-dir="$WALLET_DIR" \
+    --monitoring-host 0.0.0.0 \
+    --beacon-rpc-provider="$BEACON_RPC_PROVIDER" \
+    --beacon-rpc-gateway-provider="$BEACON_RPC_GATEWAY_PROVIDER" \
+    --validators-external-signer-url="$WEB3SIGNER_API" \
+    --grpc-gateway-host=0.0.0.0 \
+    --grpc-gateway-port="$VALIDATOR_PORT" \
+    --grpc-gateway-corsdomain=http://0.0.0.0:"$VALIDATOR_PORT" \
+    --graffiti="$GRAFFITI" \
+    --suggested-fee-recipient="${FEE_RECIPIENT_ADDRESS}" \
+    --web \
+    --accept-terms-of-use \
+    ${EXTRA_OPTS}
