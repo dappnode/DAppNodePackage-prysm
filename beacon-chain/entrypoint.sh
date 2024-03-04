c017b4cf7f87819d97a1b029105f5ecc31d0ecfb
dappnodedev <diego@dappnode.io>
2024-03-04 17:55:24 +0100

    Improve pkg build

--- beacon-chain/entrypoint.sh
+++ beacon-chain/entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
-if [[ -n $CHECKPOINT_SYNC_URL ]]; then
+if [ -n "$CHECKPOINT_SYNC_URL" ]; then
   EXTRA_OPTS="--checkpoint-sync-url=${CHECKPOINT_SYNC_URL} --genesis-beacon-api-url=${CHECKPOINT_SYNC_URL} ${EXTRA_OPTS}"
-else
-  EXTRA_OPTS="--genesis-state=/genesis.ssz ${EXTRA_OPTS}"
 fi
 
 case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET in
@@ -25,31 +23,25 @@ case $_DAPPNODE_GLOBAL_EXECUTION_CLIENT_MAINNET in
   ;;
 esac
 
-apt update
-apt install -y curl
-
-# MEVBOOST: https://hackmd.io/@prysmaticlabs/BJeinxFsq
-if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" == "true" ]; then
+if [ -n "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" ] && [ "$_DAPPNODE_GLOBAL_MEVBOOST_MAINNET" = "true" ]; then
   echo "MEVBOOST is enabled"
   MEVBOOST_URL="http://mev-boost.mev-boost.dappnode:18550"
   EXTRA_OPTS="--http-mev-relay=${MEVBOOST_URL} ${EXTRA_OPTS}"
 fi
 
-# Chek the env FEE_RECIPIENT_ADDRESS has a valid ethereum address if not set to the null address
-if [ -n "$FEE_RECIPIENT_ADDRESS" ] && [[ "$FEE_RECIPIENT_ADDRESS" =~ ^0x[a-fA-F0-9]{40}$ ]]; then
+if [ -n "$FEE_RECIPIENT_ADDRESS" ] && echo "$FEE_RECIPIENT_ADDRESS" | grep -Eq '^0x[a-fA-F0-9]{40}$'; then
   echo "FEE_RECIPIENT is valid"
 else
   echo "FEE_RECIPIENT is not a valid ethereum address, setting it to the null address"
   FEE_RECIPIENT_ADDRESS="0x0000000000000000000000000000000000000000"
 fi
 
-# If EXTRA_OPTS does not include flag --suggested-fee-recipient, append it
-if [[ $EXTRA_OPTS != *"suggested-fee-recipient"* ]]; then
+if ! echo "$EXTRA_OPTS" | grep -q -- "--suggested-fee-recipient"; then
   echo "Adding --suggested-fee-recipient=${FEE_RECIPIENT_ADDRESS} to EXTRA_OPTS"
   EXTRA_OPTS="--suggested-fee-recipient=${FEE_RECIPIENT_ADDRESS} ${EXTRA_OPTS}"
 fi
 
-exec -c beacon-chain \
+exec -c /beacon-chain \
   --datadir=/data \
   --rpc-host=0.0.0.0 \
   --accept-terms-of-use \
@@ -59,7 +51,7 @@ exec -c beacon-chain \
   --p2p-tcp-port=$P2P_TCP_PORT \
   --p2p-udp-port=$P2P_UDP_PORT \
   --execution-endpoint=$HTTP_ENGINE \
-  --grpc-gateway-port=3500 \
+  --grpc-gateway-port=$VALIDATOR_PORT \
   --grpc-gateway-corsdomain=$CORSDOMAIN \
-  --jwt-secret=/jwtsecret \
+  --jwt-secret=$JWT_PATH \
   $EXTRA_OPTS
